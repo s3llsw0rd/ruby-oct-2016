981fb4cabfc70ce89feaad1b13ad816c873af468
kenkoolio <kensoko0l@yahoo.com>
2016-10-03 18:11:56 -0500

    added range assignment

---
+++ ken_nguyen/assignments/Ruby/range.rb
@@ -0,0 +1,22 @@
+#range assignment
+
+#.member?(val) ==> true or false
+# j = ("a".."z").member?("k")
+# puts j
+
+#.include?(value) ==> true or false
+# j = ("a".."z").include?("k")
+# puts j
+#exactly like .member? ? yes
+
+#.last ==> returns the last object in range
+# j = ("a".."z").last
+# puts j
+
+#.max ==> returns the maximum value in range
+# j = ("a".."z").max
+# puts j
+
+#.min ==> returns the minimum value in range
+# j = ("a".."z").min
+# puts j
