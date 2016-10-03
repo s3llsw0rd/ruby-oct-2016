b89bb6706f7aeb07a003560ecf9502f4919124d5
Jessie Smith <jessie.smith7543@gmail.com>
2016-10-03 16:32:08 -0500

    "Jessie_Smith_Arrays"

---
+++ Jessie_Smith/Assignments/ror_array copy.rb
@@ -0,0 +1,26 @@
+a = ["Matz", "Guido", "Dojo", "Choi", "John"]
+b = [5, 6, 9, 3, 1, 2, 4, 7, 8, 10]
+c = ["Dojo", 9]
+
+ puts a.fetch(3)
+
+ puts b.delete("6")
+  
+ puts ["a", "b", "c"].reverse
+
+ puts a.length
+
+ puts a.sort
+
+ puts c.slice!(1)
+
+ puts b.shuffle
+
+ puts ["Dojo", 9].join
+
+ puts a.insert(1, "I love lamp")
+
+puts b.values_at(2,4,6)
+
+
+
