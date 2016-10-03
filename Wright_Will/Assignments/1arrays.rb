8761363122f12c840baa718d5c4dc1a7584caba3
Will Wright <wawwright@gmail.com>
2016-10-03 15:58:58 -0500

    added folders and first 5 assignments

---
+++ Wright_Will/Assignments/1arrays.rb
@@ -0,0 +1,24 @@
+# RUBY!!!!
+arr = [0,1,2,3,4,56,7,8,9]
+# puts arr.at(5)
+# puts "0_______0"
+# puts arr.fetch(5)
+# puts arr.reverse
+# puts arr
+# puts arr.reverse!
+# puts arr
+# puts arr.length
+# puts arr.sort
+# puts arr
+# puts arr.sort!
+# puts arr
+# puts arr.slice(0..2)
+# puts arr.slice(0...2)
+# puts arr.shuffle
+# puts arr
+# puts arr.shuffle!
+# puts arr
+# puts [0,1,"foo"].join("-")
+# puts arr.insert(1,"foo")
+# puts "--"
+# puts arr
