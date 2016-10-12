52e7f85231fe7fdd88efa81b48360719dc5b2e72
Will Wright <wawwright@gmail.com>
2016-10-12 15:34:24 -0500

    finished user mangement system II looking pretty good

--- Wright_Will/Assignments/rails1/controllers/User_M_Sys/app/controllers/ums_controller.rb
+++ Wright_Will/Assignments/rails1/controllers/User_M_Sys/app/controllers/ums_controller.rb
@@ -1,6 +1,25 @@
 class UmsController < ApplicationController
     def index
         @users = User.all
-        puts @users.first.email_address
     end
+
+    def new
+    end
+
+    def create
+        @new_user = User.new(user_params)
+        if @new_user.valid?
+            @new_user.save
+            redirect_to '/users'
+        else
+            flash[:errors] = @new_user.errors.full_messages
+            session[:new_user] = params
+            redirect_to '/users/new'
+        end
+    end
+    private
+        def user_params
+            params.permit(:first_name, :last_name, :email_address, :password)
+        end
+
 end
--- Wright_Will/Assignments/rails1/controllers/User_M_Sys/config/routes.rb
+++ Wright_Will/Assignments/rails1/controllers/User_M_Sys/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     root "ums#index"
     get "/users" => "ums#index"
+    post "/users" => "ums#create"
     get "/users/new" => "ums#new"
 end
