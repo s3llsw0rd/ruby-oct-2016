a6e4ea13e69b02fa9e8d49a474803dfeb94564ce
Will Wright <wawwright@gmail.com>
2016-10-13 17:51:17 -0500

    finished user management sys

--- Wright_Will/Assignments/rails1/controllers/User_M_Sys/app/controllers/ums_controller.rb
+++ Wright_Will/Assignments/rails1/controllers/User_M_Sys/app/controllers/ums_controller.rb
@@ -1,21 +1,42 @@
 class UmsController < ApplicationController
+    # layout false
     def index
         @users = User.all
     end
 
     def new
+        session[:new_user] = {} unless session[:new_user]
     end
 
     def create
         @new_user = User.new(user_params)
         if @new_user.valid?
             @new_user.save
-            redirect_to '/users'
+            redirect_to users_path
         else
             flash[:errors] = @new_user.errors.full_messages
             session[:new_user] = params
-            redirect_to '/users/new'
+            redirect_to new_user_path
         end
+    end
+    def show
+        @user = User.find(params[:id])
+    end
+    def edit
+        session[:user] =  User.find(params[:id])
+    end
+    def update
+        session[:user] = User.find(params[:id])
+        if  session[:user].update(user_params)
+            redirect_to show_user_path(session[:user][:id].to_i)
+        else
+            flash[:errors] = session[:user].errors.full_messages
+            redirect_to edit_user_path(session[:user][:id].to_i)
+        end
+    end
+    def destroy
+        User.find(params[:id]).delete
+        redirect_to users_path
     end
     private
         def user_params
--- Wright_Will/Assignments/rails1/controllers/User_M_Sys/config/routes.rb
+++ Wright_Will/Assignments/rails1/controllers/User_M_Sys/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
     root "ums#index"
-    get "/users" => "ums#index"
-    post "/users" => "ums#create"
-    get "/users/new" => "ums#new"
+    get "/users" => "ums#index" , as: :users
+    post "/users" => "ums#create" , as: :create_user
+    get "/users/new" => "ums#new", as: :new_user
+    get "/users/:id" => "ums#show", as: :show_user
+    get "/users/:id/edit" => "ums#edit", as: :edit_user
+    patch "/users/:id" => "ums#update", as: :update_user
+    delete "/users/:id" => "ums#destroy", as: :delete_user
 end
